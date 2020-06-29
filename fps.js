556d08de5c4905c6322c3ecb95dff9142c8d3820
Gustav Svensk <gustav.svensk95@gmail.com>
2020-06-29 19:19:07 +0200

    format file

--- fps.js
+++ fps.js
@@ -1,28 +1,30 @@
 // shim layer with setTimeout fallback
-window.requestAnimFrame = (function(){
-    return  window.requestAnimationFrame       ||
-            window.webkitRequestAnimationFrame ||
-            window.mozRequestAnimationFrame    ||
-            window.ieRequestAnimationFrame     ||
-            function( callback ){
-              window.setTimeout(callback, 1000 / 60);
-            };
-  })();
-  
-  var fpsElement = document.getElementById("fps");
-  
-  var then = Date.now() / 1000;  // get time in seconds
-  var render = function() {
-      var now = Date.now() / 1000;  // get time in seconds
-      
-      // compute time since last frame
-      var elapsedTime = now - then;
-      then = now;
-      
-      // compute fps
-      var fps = 1 / elapsedTime;
-      fpsElement.innerText = fps.toFixed(2);  
-      
-      requestAnimFrame(render);
-  };
-  render();
+window.requestAnimFrame = (function () {
+  return (
+    window.requestAnimationFrame ||
+    window.webkitRequestAnimationFrame ||
+    window.mozRequestAnimationFrame ||
+    window.ieRequestAnimationFrame ||
+    function (callback) {
+      window.setTimeout(callback, 1000 / 60);
+    }
+  );
+})();
+
+var fpsElement = document.getElementById("fps");
+
+var then = Date.now() / 1000; // get time in seconds
+var render = function () {
+  var now = Date.now() / 1000; // get time in seconds
+
+  // compute time since last frame
+  var elapsedTime = now - then;
+  then = now;
+
+  // compute fps
+  var fps = 1 / elapsedTime;
+  fpsElement.innerText = fps.toFixed(2);
+
+  requestAnimFrame(render);
+};
+render();
