069e697a6a575a68264c3fdbeb934a7032bcb25b
Gustav Svensk <gustav.svensk95@gmail.com>
2020-07-05 13:51:19 +0200

    calculate fps as avg over last 3 seconds

--- fps.js
+++ fps.js
@@ -6,7 +6,7 @@ window.requestAnimFrame = (function () {
     window.mozRequestAnimationFrame ||
     window.ieRequestAnimationFrame ||
     function (callback) {
-      window.setTimeout(callback, 1000 / 60);
+      window.setTimeout(callback, 100);
     }
   );
 })();
@@ -14,17 +14,28 @@ window.requestAnimFrame = (function () {
 var fpsElement = document.getElementById("fps");
 
 var then = Date.now() / 1000; // get time in seconds
+const FPSs = [];
+let counter = 0;
 var render = function () {
   var now = Date.now() / 1000; // get time in seconds
-
+  counter++;
   // compute time since last frame
   var elapsedTime = now - then;
   then = now;
 
   // compute fps
-  var fps = 1 / elapsedTime;
-  fpsElement.innerText = fps.toFixed(2);
-
+  var fps = elapsedTime !== 0 ? 1 / elapsedTime : 1;
+  FPSs.unshift(fps);
+  if (FPSs.length < 30) {
+    requestAnimFrame(render);
+    return;
+  }
+  FPSs.pop();
+  if (counter % 10 === 0) {
+    avgFpsOfLast10Frames = FPSs.reduce((a, b) => a + b) / FPSs.length;
+    console.log("Avg fps of last 30 frames: ", avgFpsOfLast10Frames);
+    fpsElement.innerText = avgFpsOfLast10Frames.toFixed(2);
+  }
   requestAnimFrame(render);
 };
 render();
